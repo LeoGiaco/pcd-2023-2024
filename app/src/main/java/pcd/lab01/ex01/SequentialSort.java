ebb95dc871c4d25a0ac8d9c1a331b83b3f697e0a
giacomo.bertuccioli <giacomo.bertuccioli@studio.unibo.it>
2024-03-04 13:54:11 +0100

    Implement parallel sort

--- app/src/main/java/pcd/lab01/ex01/SequentialSort.java
+++ app/src/main/java/pcd/lab01/ex01/SequentialSort.java
@@ -4,21 +4,35 @@ import java.util.*;
 
 public class SequentialSort {
 
-	static final int VECTOR_SIZE = 400000000;
-	
-	public static void main(String[] args) {
-	
+	static final int VECTOR_SIZE = 200000000;
+
+	public static void main(String[] args) throws InterruptedException {
+
 		log("Generating array...");
 		long[] v = genArray(VECTOR_SIZE);
-		
+
 		log("Array generated.");
 		log("Sorting (" + VECTOR_SIZE + " elements)...");
-	
-		long t0 = System.nanoTime();		
+
+		long t0 = System.nanoTime();
+
+		int nThreads = 8;
+		List<Thread> threads = new ArrayList<>(nThreads);
+		for (int i = 0; i < nThreads; i++) {
+			int finalI = i;
+			Thread t = new Thread(() -> Arrays.sort(v, finalI * v.length / nThreads, (finalI + 1) * v.length / nThreads));
+			threads.add(t);
+			t.start();
+		}
+
+		for (int i = 0; i < nThreads; i++) {
+			threads.get(i).join();
+		}
+
 		Arrays.sort(v, 0, v.length);
 		long t1 = System.nanoTime();
 		log("Done. Time elapsed: " + ((t1 - t0) / 1000000) + " ms");
-		
+
 		// dumpArray(v);
 	}
 
